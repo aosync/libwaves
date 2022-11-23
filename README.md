a4259556617abb5a301aeaca81b42328c5b67145
Alejandro Sior <aho@sior.be>
2022-11-23 15:27:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# libwaves
+
+Python library to manipulate linear combinations of harmonic oscillators
+
+
+## Example
+
+```py
+import numpy as np
+from libwaves import *
+
+n = 3
+omega = 8/60*2*np.pi
+
+nU1 = 200*1000
+nU2 = 150*1000
+nU3 = 20*1000
+nU4 = 2*1000
+
+# Define input signal
+Fx = -nU1*(Cos(n*omega) + Sin(n*omega)) - nU2*(Cos(2*n*omega) + Sin(2*n*omega)) - nU3*(Cos(3*n*omega) + Sin(3*n*omega)) - nU4*(Cos(4*n*omega) + Sin(4*n*omega))
+
+# Low pass filter with cutoff 1 Hz
+H = lambda w: 1.0 if np.abs(w) < 1*2*np.pi else 0.0
+
+# Calculate output signal by multiplicating each oscillator coefficient
+# with corresponding return value of function
+Xx = Fx.convofrf(H)
+
+f, Pxx = Xx.power()
+plt.stem(f/2/np.pi, Pxx)
+plt.show()
+```
---
+++ libwaves.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal
+
+class Signal:
+	def __init__(self, oscillators = {}):
+		self.oscillators = oscillators
+		
+		badkeys = []
+		for key, val in self.oscillators.items():
+			if np.abs(val) == 0.0:
+				badkeys.append(key)
+		
+		for key in badkeys:
+			del self.oscillators[key]
+	
+	def __call__(self, t):
+		r = np.complex128(0.0)
+		
+		for oscillator in self.oscillators.items():
+			v = oscillator[1] * np.exp(1.0j*oscillator[0]*t)
+		
+			r += v
+		
+		return r
+		
+	
+	def __add__(self, rhs):
+		oscillators = {}
+	
+		for oscillator in rhs.oscillators.items():
+			if not oscillator[0] in oscillators:
+				oscillators[oscillator[0]] = np.complex128(0.0)
+				
+			oscillators[oscillator[0]] += np.complex128(oscillator[1])
+		
+		for oscillator in self.oscillators.items():
+			if not oscillator[0] in oscillators:
+				oscillators[oscillator[0]] = np.complex128(0.0)
+				
+			oscillators[oscillator[0]] += np.complex128(oscillator[1])
+		
+		return Signal(oscillators)
+		
+		
+	def __radd__(self, lhs):
+		return self + lhs
+	
+	def __neg__(self):
+		return self * (-1)
+		
+	def __sub__(self, rhs):
+		return self + (-rhs)
+	
+	def __mul__(self, rhs):
+		if not issubclass(type(rhs), Signal):
+			rhs = Signal({0: np.complex128(rhs)})
+	
+		oscillators = {}
+		for lfreq in self.oscillators.keys():
+			for rfreq in rhs.oscillators.keys():
+				nfreq = lfreq + rfreq
+				
+				if not nfreq in oscillators:
+					oscillators[nfreq] = np.complex128(0)
+				
+				oscillators[nfreq] += self.oscillators[lfreq] * rhs.oscillators[rfreq]
+		
+		return Signal(oscillators)
+	
+	def __rmul__(self, scalar):
+		return self * scalar
+	
+	def __truediv__(self, scalar):
+		return self * (1/scalar)
+	
+	def freqs(self):
+		return np.array(list(self.oscillators.keys())), np.array(list(self.oscillators.values()))
+	
+	def realfreqs(self):
+		foldback = {}
+		
+		for freq, val in self.oscillators.items():
+			if not np.abs(freq) in foldback:
+				foldback[np.abs(freq)] = np.complex128(0)
+				
+			foldback[np.abs(freq)] += val
+		
+		return np.array(list(foldback.keys())), np.array(list(foldback.values()))
+			
+	
+	def power(self):
+		f, Fxx = self.freqs()
+		return f, np.abs(Fxx)**2
+	
+	def realpower(self):
+		f, Fxx = self.realfreqs()
+		return f, np.abs(Fxx)**2
+	
+	def convofrf(self, frf):
+		oscillators = {}
+		
+		for oscillator in self.oscillators:
+			oscillators[oscillator[0]] = oscillator[1] * frf(oscillator[0])
+		
+		return Signal(oscillators)
+	
+class Exp(Signal):
+	def __init__(self, freq):
+		super().__init__({np.float128(freq): 1.0})
+
+def Sin(freq):
+	a = Exp(freq)
+	b = Exp(-freq)
+	
+	return (a - b) / (2.0j)
+
+def Cos(freq):
+	a = Exp(freq)
+	b = Exp(-freq)
+	
+	return (a + b) / (2.0)
